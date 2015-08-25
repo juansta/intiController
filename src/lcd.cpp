507365b7d6291ae9863e6bc6053dcc8397c03c30
Juan Aguero <juan.aguero@acousticresearch.com.au>
2015-08-25 16:50:23 +1000

    clean up lcd

--- src/lcd.cpp
+++ src/lcd.cpp
@@ -44,8 +44,9 @@ Lcd::Lcd()
     m_i2cAddress   (0x78),
     m_cmdDelay     (CMD_DELAY),
     m_charDelay    (CHAR_DELAY),
-    m_initialised  (false),
-    m_backlightPin (-1)
+    m_red          (0),
+    m_green        (0),
+    m_blue         (0)
 {
     init();
 }
@@ -67,8 +68,6 @@ void Lcd::init ()
     i2c_write(0x01);  // Clear display
     i2c_write(0x06);  // Entry mode set - increment
     i2c_stop();
-
-    m_initialised = true;
 }
 
 
@@ -81,70 +80,52 @@ void Lcd::setDelay (int cmdDelay,int charDelay)
 
 void Lcd::command(uint8_t value)
 {
-    // If the LCD has been initialised correctly, write to it
-    if (m_initialised)
-    {
-        i2c_start(m_i2cAddress);
-        i2c_write(DISP_CMD);
-        i2c_write(value);
-        i2c_stop ();
-        delay_ms(m_cmdDelay);
-    }
+    i2c_start(m_i2cAddress);
+    i2c_write(DISP_CMD);
+    i2c_write(value);
+    i2c_stop ();
+    delay_ms (m_cmdDelay);
 }
 
 
 void Lcd::write(uint8_t value)
 {
-    // If the LCD has been initialised correctly write to it
-    if (m_initialised)
+    // If it is a new line, set the cursor to the next line (1,0)
+    if (value == '\n')
+        setCursor(1,0);
+    else
     {
-
-        // If it is a new line, set the cursor to the next line (1,0)
-        if (value == '\n')
-            setCursor(1,0);
-        else
-        {
-            i2c_start(m_i2cAddress);
-            i2c_write(RAM_WRITE_CMD);
-            i2c_write(value);
-            i2c_stop ();
-            delay_ms(m_charDelay);
-        }
+        i2c_start(m_i2cAddress);
+        i2c_write(RAM_WRITE_CMD);
+        i2c_write(value);
+        i2c_stop ();
+        delay_ms (m_charDelay);
     }
 }
 
 void Lcd::write(const uint8_t *buffer, size_t size)
 {
-    // If the LCD has been initialised correctly, write to it
-    if (m_initialised)
-    {
-        i2c_start(m_i2cAddress);
-        i2c_write(RAM_WRITE_CMD);
-        for (size_t i = 0; i < size; i++, buffer++)
-            i2c_write(*buffer);
-        i2c_stop();
-    }
+    i2c_start(m_i2cAddress);
+    i2c_write(RAM_WRITE_CMD);
+    for (size_t i = 0; i < size; i++, buffer++)
+        i2c_write(*buffer);
+    i2c_stop();
 }
 
 void Lcd::write(const char *buffer, ...)
 {
-    // If the LCD has been initialised correctly, write to it
-    if (m_initialised)
-    {
-        // Contains debug messages. Not static, as we need to be re-entrant...
-        char    msg_buf[40];
-        va_list ap;
-        va_start(ap, buffer);
-        int n = vsnprintf(msg_buf, 40, buffer, ap);
+    char    msg_buf[40];
+    va_list ap;
+    va_start(ap, buffer);
+    int n = vsnprintf(msg_buf, 40, buffer, ap);
 
-        i2c_start(m_i2cAddress);
-        i2c_write(RAM_WRITE_CMD);
-        for (uint8_t i = 0; i < n; i++)
-            i2c_write(msg_buf[i]);
-        i2c_stop();
+    i2c_start(m_i2cAddress);
+    i2c_write(RAM_WRITE_CMD);
+    for (uint8_t i = 0; i < n; i++)
+        i2c_write(msg_buf[i]);
+    i2c_stop();
 
-        va_end(ap);
-    }
+    va_end(ap);
 }
 
 
@@ -196,64 +177,57 @@ void Lcd::setCursor(uint8_t line_num, uint8_t x)
 {
     uint8_t base = 0x00;
 
-    // If the LCD has been initialised correctly, write to it
-    if (m_initialised)
-    {
-        // set the baseline address with respect to the number of lines of
-        // the display
-        base = dram_dispAddr[m_num_lines - 1][line_num];
-        base = SET_DDRAM_CMD + base + x;
-        command(base);
-    }
-}
-
-uint8_t Lcd::status()
-{
-    return m_status;
+    // set the baseline address with respect to the number of lines of
+    // the display
+    base = dram_dispAddr[m_num_lines - 1][line_num];
+    base = SET_DDRAM_CMD + base + x;
+    command(base);
 }
 
 void Lcd::load_custom_character (uint8_t char_num, uint8_t *rows)
 {
-    // If the LCD has been initialised correctly start writing to it
-    if (m_initialised)
+    // If it is a valid place holder for the character, write it into the
+    // display's CGRAM
+    if (char_num < MAX_USER_CHARS)
     {
-        // If it is a valid place holder for the character, write it into the
-        // display's CGRAM
-        if (char_num < MAX_USER_CHARS)
+        // Set up the display to write into CGRAM - configure LCD to use func table 0
+        i2c_start(m_i2cAddress);
+        i2c_write (DISP_CMD);
+        i2c_write (FUNC_SET_TBL0); // Function set: 8 bit, 2 line display 5x8, funct tab 0
+        delay_ms(m_cmdDelay);
+
+        // Set CGRAM position to write
+        i2c_write (RAM_WRITE_CMD + (PIXEL_ROWS_PER_CHAR * char_num));
+        i2c_stop ();
+
+        // If we have changed the function table and configured the CGRAM position
+        // write the new character to the LCD's CGRAM
         {
-            // Set up the display to write into CGRAM - configure LCD to use func table 0
-            i2c_start(m_i2cAddress);
-            i2c_write (DISP_CMD);
-            i2c_write (FUNC_SET_TBL0); // Function set: 8 bit, 2 line display 5x8, funct tab 0
-            delay_ms(m_cmdDelay);
+            write(rows, PIXEL_ROWS_PER_CHAR); // write the character to CGRAM
 
-            // Set CGRAM position to write
-            i2c_write (RAM_WRITE_CMD + (PIXEL_ROWS_PER_CHAR * char_num));
-            i2c_stop ();
-
-            // If we have changed the function table and configured the CGRAM position
-            // write the new character to the LCD's CGRAM
-            {
-                write(rows, PIXEL_ROWS_PER_CHAR); // write the character to CGRAM
-
-                // Leave the LCD as it was - function table 1 DDRAM and set the cursor
-                // position to (0, 0) to start writing.
-                command  (FUNC_SET_TBL1);
-                setCursor(0,0);
-            }
+            // Leave the LCD as it was - function table 1 DDRAM and set the cursor
+            // position to (0, 0) to start writing.
+            command  (FUNC_SET_TBL1);
+            setCursor(0,0);
         }
     }
 }
 
-
-void Lcd::setBacklight(uint8_t new_val)
+void Lcd::backlight_on()
+{}
+void Lcd::backlight_off()
+{}
+void Lcd::getRgb(uint8_t&red, uint8_t&green, uint8_t&blue)
 {
-    // set backlight brightness
+    red   = m_red;
+    green = m_green;
+    blue  = m_blue;
 }
-
 void Lcd::setRgb(uint8_t r,uint8_t g,uint8_t b)
 {
-    // set backlight colour
+    // configure HW PWM
+
+    // store new values to eeprom
 }
 
 void Lcd::setContrast(uint8_t new_val)
