ede5c779c44f866aa3ba408aa8e27128fb8a7e28
Juan Aguero <juan.aguero@acousticresearch.com.au>
2015-09-22 15:21:27 +1000

    added led controllign class

---
+++ src/led.cpp
@@ -0,0 +1,29 @@
+/*
+ * This file is part of intiLED.
+ *
+ * intiLED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * intiLED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with intiLED.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Original file built from https://github.com/jcw/rtclib
+ *
+ */
+
+#include <avr/io.h>
+#include <led.h>
+
+Led::Led()
+{
+    for (int i = 0; i < 7; i++)
+        m_dimmers.push_back(new Dimmer(i));
+}
+
